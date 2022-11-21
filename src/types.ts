2594c0508dfb9f6911858242f2364030e25372c7
Mathias Lenz <ithildid@gmail.com>
2022-11-21 08:48:29 +0100

    expanded criteria catalogue

--- src/types.ts
+++ src/types.ts
@@ -66,48 +66,55 @@ export type testResults = {
 export const criteriaCatalogue: componentCriteria[] = [
     {
         component: "Dialog",
+        // TODO add alternative names field
         criteria: [
             {
-                testMode: "Screenreader",
-                instructions: "",
-                videoLink: "",
+                testMode: "Keyboard",
+                instructions: "Lorem Ipsum Instruktionen",
+                videoLink: "link video",
                 additionalHint: "",
                 criteria: [{
-                    _id: "123",
-                    text: "focus style is visible",
+                    _id: "1",
+                    text: "The focus (style) is visible",
                     help: "Hier steht hilfetext",
                 },
                 {
-                    _id: "1243",
-                    text: "buttons are focusable",
+                    _id: "2",
+                    text: "Buttons are focusable",
                     help: "Hier steht hilfetext",
                 },
                 {
-                    _id: "12325",
-                    text: "Other Things are focusable",
+                    _id: "3",
+                    text: "The focus is trapped inside the dialog",
+                    help: "Hier steht hilfetext",
+                },
+                {
+                    _id: "4",
+                    text: "The dialog closes with the `esc` key",
+                    help: "Hier steht hilfetext",
+                },
+                {
+                    _id: "5",
+                    text: "The focus returns to the calling button after closing or canceling the dialog with `esc`",
                     help: "Hier steht hilfetext",
                 },]
             },
             {
-                testMode: "Keyboard",
-                instructions: "",
+                testMode: "Screenreader",
+                instructions: "Lorem Ipsum Instruktionen",
                 videoLink: "",
                 additionalHint: "",
                 criteria: [{
-                    _id: "123",
-                    text: "focus style is visible",
+                    _id: "11",
+                    text: "The `[x]` close button has alt-text",
                     help: "Hier steht hilfetext",
                 },
                 {
-                    _id: "1243",
-                    text: "buttons are focusable",
+                    _id: "12",
+                    text: "The content of the dialog is read upon opening",
                     help: "Hier steht hilfetext",
                 },
-                {
-                    _id: "12325",
-                    text: "Other Things are focusable",
-                    help: "Hier steht hilfetext",
-                },]
+                ]
             },
 
         ]
@@ -117,43 +124,83 @@ export const criteriaCatalogue: componentCriteria[] = [
         criteria: [
             {
                 testMode: "Screenreader",
-                instructions: "",
+                instructions: "Lorem Ipsum Instruktionen",
                 videoLink: "",
                 additionalHint: "",
                 criteria: [{
-                    _id: "123",
-                    text: "focus style is visible",
+                    _id: "21",
+                    text: "Any hidden text is not read",
                     help: "Hier steht hilfetext",
                 },
-                {
-                    _id: "1243",
-                    text: "buttons are focusable",
-                    help: "Hier steht hilfetext",
-                },
-                {
-                    _id: "12325",
-                    text: "Other Things are focusable",
-                    help: "Hier steht hilfetext",
-                },]
+                ]
             },
             {
                 testMode: "Keyboard",
-                instructions: "",
+                instructions: "Lorem Ipsum Instruktionen",
                 videoLink: "",
                 additionalHint: "",
                 criteria: [{
-                    _id: "123",
-                    text: "focus style is visible",
+                    _id: "31",
+                    text: "The header elements are focusable",
                     help: "Hier steht hilfetext",
                 },
                 {
-                    _id: "1243",
-                    text: "buttons are focusable",
+                    _id: "32",
+                    text: "The focus (style) is visible",
                     help: "Hier steht hilfetext",
                 },
                 {
-                    _id: "12325",
-                    text: "Other Things are focusable",
+                    _id: "33",
+                    text: "The panel opens/closes with `space` or `enter` upon focus of a header element",
+                    help: "Hier steht hilfetext",
+                },
+                {
+                    _id: "34",
+                    text: "Navigation between panel headers is possible with `tab`",
+                    help: "Hier steht hilfetext",
+                },]
+            },
+
+        ]
+    },
+    {
+        component: "Tooltip",
+        criteria: [
+            {
+                testMode: "Screenreader",
+                instructions: "Lorem Ipsum Instruktionen",
+                videoLink: "",
+                additionalHint: "",
+                criteria: [{
+                    _id: "41",
+                    text: "The tooltip is read upon focus of the element",
+                    help: "Hier steht hilfetext",
+                },
+                ]
+            },
+            {
+                testMode: "Keyboard",
+                instructions: "Lorem Ipsum Instruktionen",
+                videoLink: "",
+                additionalHint: "",
+                criteria: [{
+                    _id: "51",
+                    text: "The element with the tooltip is focusable",
+                    help: "Hier steht hilfetext",
+                },
+                {
+                    _id: "52",
+                    text: "The focus (style) is visible",
+                    help: "Hier steht hilfetext",
+                },
+                {
+                    _id: "53",
+                    text: "The tooltip is shown upon focus",
+                    help: "Hier steht hilfetext",
+                },
+                {
+                    _id: "54",
+                    text: "The tooltip closes with the `esc` key",
                     help: "Hier steht hilfetext",
                 },]
             },
