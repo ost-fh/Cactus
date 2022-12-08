fbf02181b992e6422ebc5276bf5a0384e2ffe265
Mathias Lenz <ithildid@gmail.com>
2022-12-08 18:09:28 +0100

    added alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,10 +3,13 @@ import "./alert.css";
 
 type AlertProps = {
   message: string;
+  /** Allowed types: error, info, help */
+  type?: string;
 };
 
-const Alert = (props: AlertProps) => {
-  return <div className='alert-info'>{props.message}</div>;
+const Alert = ({ message, type = "info" }: AlertProps) => {
+  if (type !== ("info" || "error" || "help")) throw new Error();
+  return <div className={`alert-${type} alert`}>{message}</div>;
 };
 
 export default Alert;
