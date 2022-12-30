be068b5be73f28e17e6b5a1fdac8bf9f929725dc
Mathias Lenz <ithildid@gmail.com>
2022-12-30 17:37:39 +0100

    css prettier updates

--- src/services/useToken.ts
+++ src/services/useToken.ts
@@ -1,25 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const useUserData = () => {
-    const getUserData = () => {
-        const userDataString = sessionStorage.getItem('userData');
-        if (userDataString) {
-            const userData = JSON.parse(userDataString);
-            return userData
-        } else {
-            return undefined;
-        }
-    };
-    const [userData, setUserData] = useState(getUserData());
-    const saveUserData = (userData: any) => {
-        if (userData === undefined) {
-            sessionStorage.removeItem('userData')
-        } else { (sessionStorage.setItem('userData', JSON.stringify(userData))) }
-
-        setUserData(userData);
-    };
-    return {
-        setUserData: saveUserData,
-        userData
+  const getUserData = () => {
+    const userDataString = sessionStorage.getItem("userData");
+    if (userDataString) {
+      const userData = JSON.parse(userDataString);
+      return userData;
+    } else {
+      return undefined;
     }
-}
+  };
+  const [userData, setUserData] = useState(getUserData());
+  const saveUserData = (userData: any) => {
+    if (userData === undefined) {
+      sessionStorage.removeItem("userData");
+    } else {
+      sessionStorage.setItem("userData", JSON.stringify(userData));
+    }
+
+    setUserData(userData);
+  };
+  return {
+    setUserData: saveUserData,
+    userData,
+  };
+};
