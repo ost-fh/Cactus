df7d33a9ad3ed6a97917b4bea18f4f8122bc751e
Mathias Lenz <ithildid@gmail.com>
2022-12-30 17:01:30 +0100

    added icons

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,20 +1,33 @@
 import React from "react";
+import {
+  BsFillExclamationOctagonFill,
+  BsFillExclamationTriangleFill,
+  BsInfoCircleFill,
+} from "react-icons/bs";
 import "./alert.css";
 
 type AlertProps = {
   title?: string;
-  message: string;
+  message?: string;
   /** Allowed types: error, info, help */
   type?: string;
+  children?: any;
 };
 
-const Alert = ({ title, message, type = "info" }: AlertProps) => {
+const Alert = ({ title, message, type = "info", children }: AlertProps) => {
   if (type !== "info" && type !== "error" && type !== "help") {
     throw new Error("invalid alert type");
   }
+  if (children) {
+    return <div className={`alert-${type} alert`}>{children}</div>;
+  }
+
   return (
     <div className={`alert-${type} alert`}>
-      <strong>{title}</strong> {message}
+      {type === "error" && <BsFillExclamationOctagonFill />}
+      {type === "help" && <BsFillExclamationTriangleFill />}
+      {type === "info" && <BsInfoCircleFill />} <strong>{title}</strong>{" "}
+      {message}
     </div>
   );
 };
