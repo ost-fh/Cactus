85240c9141df01f8d414422d5736b0b76160f396
Mathias Lenz <ithildid@gmail.com>
2023-01-11 15:04:50 +0100

    prettier

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
-
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
   res.status(statusCode);
