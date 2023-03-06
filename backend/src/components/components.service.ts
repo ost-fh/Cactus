415ed8545f5b9e3b1ee0a986f8edd64e743ad84e
Simon <simon.hager@ost.ch>
2023-03-06 12:16:42 +0100

    separate components in component per file

--- backend/src/components/components.service.ts
+++ backend/src/components/components.service.ts
@@ -3,18 +3,29 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 import Component from './component.schema';
-
-const YAML_COMPONENTS_FILENAME = 'components.yml';
+import * as fs from 'fs';
 
 @Injectable()
 export class ComponentsService {
   private components: Component[] = [];
 
   constructor() {
-    const ymlFileContent = yaml.load(
+    const fullPath = join(__dirname);
+    fs.readdir(fullPath, (error, files) => {
+      if (error) console.log(error);
+      files
+        .filter((f) => f.endsWith('.yml') || f.endsWith('.yaml'))
+        .forEach((file) => {
+          const ymlFileContent = yaml.load(
+            readFileSync(join(__dirname, file), 'utf8'),
+          ) as Component;
+          this.components.push(ymlFileContent);
+        });
+    });
+    /*const ymlFileContent = yaml.load(
       readFileSync(join(__dirname, YAML_COMPONENTS_FILENAME), 'utf8'),
     ) as Record<'components', Component[]>;
-    this.components = ymlFileContent.components;
+    this.components = ymlFileContent.components;*/
   }
 
   async findAll(): Promise<Component[]> {
