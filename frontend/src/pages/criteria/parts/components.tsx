4305bb110dadb5fa16a68b7635eb19c170aa798a
Mathias Lenz <ithildid@gmail.com>
2023-05-23 14:07:40 +0200

    add jump functionality to criteria component

--- frontend/src/pages/criteria/parts/components.tsx
+++ frontend/src/pages/criteria/parts/components.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { ComponentCriteria } from "../../../shared/resources/types";
 import AnimatedSwitch from "./animated-switch";
+import { BsChevronDoubleDown, BsChevronDoubleUp } from "react-icons/bs";
 
 type ComponentProps = {
   component: ComponentCriteria;
+  expand?: boolean;
 };
 
-const Component = ({ component }: ComponentProps) => {
-  const [collapsed, setCollapsed] = useState(true);
+const Component = ({ component, expand = true }: ComponentProps) => {
+  const [collapsed, setCollapsed] = useState(!expand);
   return (
-    <div className='component'>
+    <div className='component' id={component.name}>
       <div className='component-header'>
         {component.name === "Switch" ? (
           <AnimatedSwitch />
@@ -26,16 +28,31 @@ const Component = ({ component }: ComponentProps) => {
             : ""
         }`}</h3>
         <button
+          className='button-with-icon'
           aria-expanded={!collapsed}
           onClick={() => setCollapsed(!collapsed)}
         >
-          {collapsed ? "Show all criteria" : "Hide Criteria"}
+          {collapsed ? (
+            <>
+              <BsChevronDoubleDown />
+              Show all criteria
+            </>
+          ) : (
+            <>
+              <BsChevronDoubleUp />
+              Hide criteria
+            </>
+          )}
         </button>
         <p>{component.description}</p>
       </div>
       {!collapsed &&
         component.testModes.map((testmode) => (
-          <div key={testmode.testMode} className='testmode'>
+          <div
+            key={testmode.testMode}
+            id={`${component.name}-${testmode.testMode}`}
+            className='testmode'
+          >
             <h4>{testmode.testMode} Criteria</h4>
             {testmode.criteria.map((criterion) => (
               <div key={criterion._id} className='criterion'>
