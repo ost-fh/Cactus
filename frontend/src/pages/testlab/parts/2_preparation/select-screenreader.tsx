eeaa72af4ebcc5697ab8374a9206b832ffbfb19e
Your Name <dev@cylenz.ch>
2023-06-05 16:38:36 +0200

    create screenreader select component

---
+++ frontend/src/pages/testlab/parts/2_preparation/select-screenreader.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import Alert from "../../../../shared/components/alert";
+
+type SelectScreenreaderProps = {
+  chosenScreenreader: string | undefined;
+  setChosenScreenreader: Function;
+};
+
+const SelectScreenreader = ({
+  chosenScreenreader,
+  setChosenScreenreader,
+}: SelectScreenreaderProps) => {
+  //   const [screenreader, setScreenreader] = useState<string>();
+  const [recoveredScreenreader, setRecoveredScreenreader] = useState(false);
+
+  useEffect(() => {
+    if (!chosenScreenreader) {
+      // look at localstorage
+      const savedScreenreader = localStorage.getItem("screenreader-setting");
+      if (savedScreenreader) {
+        setChosenScreenreader(savedScreenreader);
+        setRecoveredScreenreader(true);
+      }
+      //
+    } else {
+      //   setScreenreader(screenreader);
+    }
+  }, [chosenScreenreader, setChosenScreenreader]);
+
+  const changeScreenreader = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newScreenreader = event.target.value;
+    setChosenScreenreader(newScreenreader);
+    localStorage.setItem("screenreader-setting", newScreenreader);
+  };
+
+  return (
+    <label className='select-screenreader'>
+      What screenreader are you using? (required)
+      <select
+        required
+        name='screenreader'
+        id=''
+        value={chosenScreenreader}
+        onChange={changeScreenreader}
+      >
+        <option value='' hidden>
+          Choose one...
+        </option>
+        <option value='NVDA'>NVDA</option>
+        <option value='JAWS'>JAWS</option>
+        <option value='VoiceOver'>VoiceOver</option>
+        <option value='Narrator'>Narrator</option>
+      </select>
+      {recoveredScreenreader && (
+        <Alert
+          type='success'
+          message='Your previously used Screenreader was restored.'
+        />
+      )}
+    </label>
+  );
+};
+
+export default SelectScreenreader;
